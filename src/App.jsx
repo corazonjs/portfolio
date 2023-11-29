11377e033b858fa784ccaed7d805a04deb8c2f98
Daniyar <corazondev@mail.ru>
2023-11-29 23:57:26 +0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar.jsx'
+import Hero from "./components/Hero/Hero.jsx";
 
 
 function App() {
 
   return (
     <div className={styles.App}>
-      <Navbar />
+        <Navbar />
+        <Hero />
+
     </div>
   )
 }
